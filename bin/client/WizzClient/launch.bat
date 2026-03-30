aed91846b210d57292b1fe526d7d5bc8aaab2b02
Abdelrahmane-Amiri <abdelrahmane.amiri@laplateforme.io>
2026-03-30 15:05:46 +0200

    Add Wizz in qt

--- bin/client/WizzClient/launch.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Set Qt path
-set PATH=C:\Qt\6.10.1\mingw_64\bin;C:\Qt\Tools\mingw1120_64\bin;%PATH%
-set QT_QPA_PLATFORM_PLUGIN_PATH=%CD%\platforms
-
-REM Launch client
-WizzClient.exe
-pause
